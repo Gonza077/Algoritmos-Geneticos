f62ac04f70f3ac90074f4d9750134d05515ef82c
Gonza077 <gonzalo07.xc@gmail.com>
2021-08-27 17:09:35 -0300

    Creacion y definicion de la clase Ciudad y busqueda

--- Trabajo Practico 3/main.py
+++ Trabajo Practico 3/main.py
@@ -1,6 +1,19 @@
+from Ciudad import *
 import pandas as pd
 
-hojaExcel = pd.read_excel('./TablaCapitales.xlsx')
+def cargarCiudades():
+    ciudades=[]
+    hojaExcel = pd.read_excel('./TablaCapitales.xlsx')
+    for fila in hojaExcel.values:
+        ciudad=Ciudad()
+        ciudad.agregarNombre(fila[0])
+        for j in range(1,len(fila)):
+            ciudad.agregarCiudad(hojaExcel.columns[j], fila[j])
+        ciudades.append(ciudad)
+    return ciudades
 
-print(hojaExcel.values)
-print(hojaExcel.columns)
+ciudades=cargarCiudades()
+
+for ciudad in ciudades:
+    ciudad.datosCiudad()
+
