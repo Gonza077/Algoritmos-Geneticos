87d9cab377716c764472c881a648dfd711ca0061
nicomateucci <nico-mateucci@hotmail.com>
2021-10-26 21:40:59 -0300

    Correcciones para que funcione directamente al clonar.

--- Trabajo Practico Integrador/Poblacion.py
+++ Trabajo Practico Integrador/Poblacion.py
@@ -5,11 +5,7 @@ import copy
 class Poblacion(object):
 
     IDPoblacion = 1
-<<<<<<< HEAD
-    CantParques=None
-=======
     tPobla=None
->>>>>>> gonza
     tipoCrossover=None
     tipoSeleccion=None
     tipoMutacion=None
@@ -21,48 +17,6 @@ class Poblacion(object):
 
     #Metodos de instancia
     def __init__(self):
-<<<<<<< HEAD
-        self.ID = Poblacion.IDPoblacion 
-        self.sumatoriaPotencia=0     
-        self.arrParques=[] 
-        Poblacion.IDPoblacion +=1 
-        
-    def instacioParques(self):
-        for _ in range(0,Poblacion.CantParques):
-            parqueEolico = ParqueEolico()
-            parqueEolico.intancioAerogeneradores()   
-            self.arrParques.append(parqueEolico)
-        ParqueEolico.reseteoIDParques()
-
-    def calculoSumaPobla(self):
-        """Se calcula la suma de la parqueEolico a partir del valor de cada cromosoma"""
-        for parque in self.arrParques:
-            self.sumatoriaPotencia += parque.potenciaParque
-
-    def calculoDatosParqueEolico(self):
-        #Calculo los datos de cada parque
-        for parque in self.arrParques:
-            parque.calculoPotenciaParque()
-        #Calculo la sumatoria de potencia de la poblacion
-        self.calculoSumaPobla()
-        #Calculo del Fitness de cada parque
-        for parque in self.arrParques:   
-            parque.calculoFitness(self.sumatoriaPotencia)
-
-    def creoNuevoParqueEolico(self,parqueEolicoAnterior):
-        parquesNuevos=[] 
-        paresPadres=self.tipoSeleccion.aplicarSeleccion(parqueEolicoAnterior,len(self.arrParques))   
-        parquesNuevos=self.tipoCrossover.aplicoCrossover(paresPadres)  #Por que a los cromosomas del elitismo, no hay que modificarlos 
-        self.tipoMutacion.aplicoMutacion(parquesNuevos)
-        for parque in parquesNuevos:
-            self.arrParques.append(cromosoma)
-    
-    def diseñoParques(self):
-        print("---------------------------------------------")
-        print(f"Poblacion N°{self.ID}\n")
-        for parque in self.arrParques:
-            parque.datosParque() 
-=======
         self._ID = Poblacion.IDPoblacion 
         self._sumaPoblacion=0     
         self._arrCromosomas=[] 
@@ -151,5 +105,4 @@ class Poblacion(object):
         print(f"Poblacion N°{self._ID}\n")
         for cromosoma in self._arrCromosomas:
             cromosoma.datosParque() 
->>>>>>> gonza
         print("---------------------------------------------")
